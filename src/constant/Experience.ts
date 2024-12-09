402fe93d34dbce8831ae30ca6dae1acbe4e582a2
Syeda Bisma Jafri <najamkurazme@gmail.com>
2024-12-09 12:16:51 -0800

    hackathon-2-figma

---
+++ src/constant/Experience.ts
@@ -0,0 +1,25 @@
+
+export const ExperienceItem = [
+    {
+     src:"/capp.png",
+      value: "420",
+      label: "Professional Chefs"
+    },
+    {
+
+        src:"/burgerr.png",
+      value: "320",
+      label: "Items Of Food"
+    },
+    {
+
+     src:"/spoon.png",
+      value: "30+",
+      label: "Years Of Experienced"
+    },
+    {  
+      src:"/pizzza.png",
+      value: "220",
+      label: "Happy Customers"
+    }
+  ]
